96995238aa0a737bbd0a4f10385901882e646c4b
katy Deng <deng2017.xq@gmail.com>
2019-02-14 22:16:50 -0800

    displaying the message

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ const io = require('socket.io')(http);
 app.get('/:user',(req,res)=>{
   res.sendFile(__dirname + '/index.html'); 
 })
-
+//2  receiving the client message
 io.on('connection', (socket)=>{
   console.log('a user connected'); 
-}); 
+  socket.on('disconnect', ()=>{
+    console.log('user disconnected'); 
+  })
+  //getting the message from the client 
+  socket.on('chat message',function(msg){
+     console.log(msg); 
+     //sending it back to the client
+     io.emit('chat message', msg);
+  })
+
+})
+
 
 http.listen(3000, ()=>{
   console.log('listening on port 3000'); 
